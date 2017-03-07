4c514f9b2b4250e104f0d3bcb461bed84cd798a3
Joseph Tang <josephtang765@gmail.com>
2017-03-07 17:45:23 +0800

    Update MESSAGE_STARTDATE_CONSTRAINTS to match new validation rule

--- src/main/java/seedu/ezdo/model/todo/StartDate.java
+++ src/main/java/seedu/ezdo/model/todo/StartDate.java
@@ -10,7 +10,7 @@ import seedu.ezdo.commons.exceptions.IllegalValueException;
 public class StartDate {
 
     public static final String MESSAGE_STARTDATE_CONSTRAINTS =
-            "Start dates should be in the format DD-MM-YYYY, and it should not be blank";
+            "Start dates should be in the format DD/MM/YYYY, and it should not be blank";
 
     public static final String STARTDATE_VALIDATION_REGEX = "^\\d{1,2}/\\d{1,2}/\\d{4}$";
 
